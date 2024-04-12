f403454154a582eec8af09acf0274d7b39a4cc09
Bruno Oliveira <brunoserrasilva@gmail.com>
2024-04-12 11:55:26 -0300

    Atualizacao do Shell Script

--- start_site.sh
+++ start_site.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Iniciando o minha home page"
+echo ""
 sudo docker build -t web-site .
-sudo docker run -dit --name web-app -p 80:80 web-site
+sudo docker run --name site-nginx -d -p 8080:80 web-site
+echo ""
 echo "Pronta para rodar !!!!"
